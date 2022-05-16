817e1e3ae001959d01f354692cde5e60abfd92f1
YildizAydogan <yildizaydoganjobmail@gmail.com>
2022-05-16 23:02:12 +0200

    2022

--- trvlcar-frontend-main/src/router/custom-routes.js
+++ trvlcar-frontend-main/src/router/custom-routes.js
@@ -1,37 +1,35 @@
-import React from 'react'
-import {Route , Routes} from "react-router-dom";
-
-
-
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutPage from "../pages/users/AboutPage";
+import AuthPage from "../pages/users/AuthPage";
+import ContactPage from "../pages/users/ContactPage";
+import HomePage from "../pages/users/HomePage";
+import VehicleDetailsPage from "../pages/users/VehicleDetailsPage";
+import VehiclesPage from "../pages/users/VehiclesPage";
+import UserTemplate from "../templates/user-template";
 
 const CustomRoutes = () => {
   return (
+    <BrowserRouter>
+       
+      <Routes>
+        <Route path="/">
+          {/* ADMIN ROUTES */}
 
+          {/* USER ROUTES */}
+          <Route index element={<UserTemplate><HomePage /></UserTemplate>} />
+          <Route path="about" element={<UserTemplate><AboutPage /></UserTemplate>} />
+          <Route path="contact" element={<UserTemplate><ContactPage /></UserTemplate>} />
+          <Route path="auth" element={<AuthPage />} />
 
-    <Routes>
-            <Route path="/">
-          {/* ADMIN ROUTES */ }
+          <Route path="vehicles">
+            <Route index element={<UserTemplate><VehiclesPage /></UserTemplate>} />
+            <Route path=":vehicleId" element={<UserTemplate><VehicleDetailsPage /></UserTemplate>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-
-
-            {/* USER ROUTES*/ }
-            <Route index element={<HomePage/>} />
-            <Route path="about" element={<AboutPage/>} />
-
-
-
-     </Route>
-
-
-
-
-
-
-
-    </Routes>
-  )
-}
-
-export default CustomRoutes
+export default CustomRoutes;
